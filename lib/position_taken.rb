d63f78ca5ecd894a21d03e86c39898652fb9f7db
Marcus Magnusson <marcusmagnusson@outlook.com>
2016-11-21 22:49:08 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
-# code your #position_taken? method here!
+
+# accepts a board and the index to check as arguments
+# returns false if the board has an empty string " " in the desired index
+# returns false if the board has an empty string "" in the index
+# returns false if the board has nil in the index
+# returns true if the board has a value of "X" or "O" in the index
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+      taken = false
+    elsif board[index] == "X" || board[index] == "O"
+      taken = true
+    end
+end
