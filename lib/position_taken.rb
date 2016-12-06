65bdc5aef3feb7f703a2059951bc12e072604abe
jenboes <jlboes@ameritech.net>
2016-12-06 20:56:16 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+def position_taken?(board,index_number)
+  # do some error checking
+  #if index_number>9 || index_number<0
+  #  return true
+  #else
+  if board[index_number] == nil
+    return false
+  elsif board[index_number]=="" || board[index_number]==" "
+      return false
+  else
+    return true
+  end
+end
