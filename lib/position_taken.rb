aaf404f9046667e1027c1302952de46e70f4b8ff
Johan Martin Seland <joma.seland@gmail.com>
2017-02-18 09:23:48 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " "
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+end
+  end 
+
+  #We need to change the content of the #position_taken? method to check to see if the given board[index] 
+  #is equal to either an empty string that looks like this " " or an empty string that looks like this "".
+  #Hint: You can use the ||, or "or" operator to check for both of the above-described conditions on the same line.
