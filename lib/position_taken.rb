3cf96aeacf664c7e90da873aaf1e9d3d1b17f6a4
Alex Segers <a.segers.97@gmail.com>
2017-02-23 23:28:30 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,4 @@
 # code your #position_taken? method here!
-board = ["", "X", " ", " ", " ", " ", " ", " ", " "]
-index = 1
 def position_taken?(board, index)
   if board[index] ==  "" || board[index] ==  " " || board[index] ==  nil
     return false
@@ -8,5 +6,3 @@ def position_taken?(board, index)
     return true
   end
 end
-puts board[1]
-position_taken?(board, index)
