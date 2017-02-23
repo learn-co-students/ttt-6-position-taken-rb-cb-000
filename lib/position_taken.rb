782f9d187f9f4e2e53444e21ee6590d5a60a13d8
Alex Segers <a.segers.97@gmail.com>
2017-02-23 23:03:04 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+board = ["", "X", " ", " ", " ", " ", " ", " ", " "]
+index = 1
+def position_taken?(board, index)
+  if board[index] ==  "" || board[index] ==  " " || board[index] ==  nil
+    return false
+  else
+    return true
+  end
+end
+puts board[1]
+position_taken?(board, index)
