4deff3eee19814257fc07c877ce1fd7545cd17b0
Daniel Vimont <daniel.vimont@gmail.com>
2017-05-02 01:05:41 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+def position_taken?(board, index_to_validate)
+  if (board[index_to_validate] == "X" || board[index_to_validate] == "O")
+    return true
+  end
+  return false # NOTE: if we arrive here, the position is definitely not taken
+end
