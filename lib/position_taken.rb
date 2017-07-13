477d50852dcba489123c2a66a06c80f2a893922f
Kelly McCain <kelly.mccain.me@gmail.com>
2017-07-13 23:55:58 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,6 @@ def position_taken?(board, position)
   if board[position] == "" || board[position] == " " || board[position] == nil
     return false
   else
-    true
+    return true
   end
   end
