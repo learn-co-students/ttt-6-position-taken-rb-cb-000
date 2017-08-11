904eee28d7ade5f4a4434b9977d094555d4b6a54
Xochitl Fierros <xochitlfm@gmail.com>
2017-08-11 06:11:07 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+board[index] == " " && board[index] == "  "
+board[index] == "X" || board[index] == "O"
+
+end
+
+
+#if the array element at the index of the desired position is equal to an empty string.
