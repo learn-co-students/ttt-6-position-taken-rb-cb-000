197cab6b792b79b4d0228b4e911e5975c76399d0
cjjski <coryjanowski@icloud.com>
2018-03-27 06:09:58 -0600

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,26 @@
 # code your #position_taken? method here!
+# 1. How do we read information out of an index in an array?
+# 2. How do we checl that element in the board array to know if it's taken?
+
+def position_taken?(board, position)
+  # if board = false "not taken"
+  # board = [" ", " ", "X"]
+  if board[position] == " "
+    false
+  elsif board[position] == ""
+    false
+  elsif board[position] == nil
+    false
+  else
+      true
+  end
+
+  #   print "you can put your piece here"
+  # else board[position]= true
+  #   print "this place is already taken"
+
+  # end
+
+
+
+end
