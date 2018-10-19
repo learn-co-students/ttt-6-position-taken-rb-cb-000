e608f33a053b482b0f95521038dc74fc207a1d0f
MOBOLA MORONTO <t2wycemo@gmail.com>
2018-10-19 18:04:58 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken?(array, num)
+  #array = [" "," ", " ", " ", " ", " ", " ", " ", " "]
+  if array[num] == " " || array[num] == "" || array[num] == nil
+    !true
+ 
+   elsif array[num] == "X" || array[num] == "O"
+   !false
+    
+    
+  end
+end  
